e2d847d79ea6a09c8c3b2d9b44c4b973848f8ae2
Enrique <enriquealexy@gmail.com>
2019-02-21 03:05:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function appendKitten(name){
 }
 
 function prependKitten(name){
-  var newArray = name.concat([" "], kittens)
+  var newArray = name.concat([" ,"], kittens)
   return newArray
 }
 
